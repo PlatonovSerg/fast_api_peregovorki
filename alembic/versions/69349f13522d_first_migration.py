6bce06e78393ac78e01505e530c0b93bd3191765
PlatonovSerg <platonov1727@gmail.com>
2024-09-12 11:00:37 +0300

    create migration

--- alembic/versions/69349f13522d_first_migration.py
+++ alembic/versions/69349f13522d_first_migration.py
@@ -5,14 +5,15 @@ Revises:
 Create Date: 2024-09-10 15:12:57.488622
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '69349f13522d'
+revision: str = "69349f13522d"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
