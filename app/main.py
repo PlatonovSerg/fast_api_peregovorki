04ac7480d4f5c470bf40285a59deca61b892674d
PlatonovSerg <platonov1727@gmail.com>
2024-09-13 14:04:42 +0300

    add router for meeting room in main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from app.core.config import settings
 from fastapi import FastAPI
+from app.api.meeting_room import router
 
 app = FastAPI(title=settings.app_title, description=settings.app_description)
+
+app.include_router(router)
